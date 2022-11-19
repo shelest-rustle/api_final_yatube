d8b375060adb8608a5b80fa2132af32ebfbc9675
Мария Шелест <mariia.schelest@gmail.com>
2022-11-19 13:39:18 +0400

    first review attempt

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import F, Q
 
 User = get_user_model()
 
@@ -11,6 +12,10 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey( 
+        'Group', on_delete=models.CASCADE, 
+        related_name='posts', blank=True, null=True 
+    ) 
 
     def __str__(self):
         return self.text
@@ -24,3 +29,32 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='follower')
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='following')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique_follow'),
+            models.CheckConstraint(
+                check=~Q(user=F('following')),
+                name='user_not_following')
+        ]
+
+    def __str__(self):
+        return f'{self.user.username} подписан на {self.following.username}'
